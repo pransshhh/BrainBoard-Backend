17d39863b35f2b49fcd387a10dacb2c0b792fcb2
Pranshu Verma <pranshuverma1601@gmail.com>
2026-04-08 16:53:34 +0530

    feat: Call connectDB() in src/index.ts startup sequence

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express, { Request, Response } from "express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { env } from "./config";
+import { connectDB } from "./config/db";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello world" });
-});
+const startServer = async () => {
+  await connectDB();
 
-app.use(errorMiddleware);
+  app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello world" });
+  });
 
-app.listen(env.PORT, () => {
-  console.log(`Listening to port ${env.PORT} in ${env.NODE_ENV} mode `);
-});
+  app.use(errorMiddleware);
+
+  app.listen(env.PORT, () => {
+    console.log(`Listening to port ${env.PORT} in ${env.NODE_ENV} mode `);
+  });
+};
+
+startServer();
