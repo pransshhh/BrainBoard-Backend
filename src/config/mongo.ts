7252d139d58b2b476b6ff3cce6d08a5cda855fcf
Pranshu Verma <pranshuverma1601@gmail.com>
2026-04-08 14:08:00 +0530

    feat: Implement MongoDB connection handler

---
+++ src/config/mongo.ts
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "./env";
+
+const uri = env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectToMongoDB() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    // Ensures that the client will close when you finish/error
+    await client.close();
+    process.exit(1);
+  }
+}
+
+export { client, connectToMongoDB };
