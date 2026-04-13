71430d88f6e930478b8b955f149cf758f06af2c8
Pranshu Verma <pranshuverma1601@gmail.com>
2026-04-13 15:13:39 +0530

    chore: update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { env, connectDB } from "./config";
+import { UserRepository } from "./repositories/userRepository";
+import { IUser } from "./models/User";
 
 const app = express();
 app.use(express.json());
 
+const userRepository = new UserRepository();
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello world" });
+});
+
+// New route to create a user
+app.post("/users", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const newUser = await userRepository.create(req.body as IUser);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// New route to get all users
+app.get("/users", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await userRepository.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use(errorMiddleware);
