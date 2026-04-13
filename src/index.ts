3bbff0b467ab9259fa063c122f7bd88ada6b4adf
Pranshu Verma <pranshuverma1601@gmail.com>
2026-04-13 15:11:56 +0530

    feat: integrate new database connection module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
-import { env } from "./config";
-import mongoose from "mongoose";
+import { env, connectDB } from "./config";
 
 const app = express();
 app.use(express.json());
@@ -12,17 +11,11 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use(errorMiddleware);
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(env.MONGODB_URI);
-    console.log("MongoDB connected");
-    app.listen(env.PORT, () => {
-      console.log(`Listening to port ${env.PORT} in ${env.NODE_ENV} mode `);
-    });
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
+const startServer = async () => {
+  await connectDB();
+  app.listen(env.PORT, () => {
+    console.log(`Listening to port ${env.PORT} in ${env.NODE_ENV} mode`);
+  });
 };
 
-connectDB();
+startServer();
